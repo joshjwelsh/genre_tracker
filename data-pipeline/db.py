aa95e87173e25eceb6cf343c522cb4ba4b228e79
Joshua Welsh <joshua.welsh.1001@gmail.com>
2023-05-06 17:40:04 -0400

    working data pipeline

--- data-pipeline/db.py
+++ data-pipeline/db.py
@@ -29,7 +29,7 @@ class DBManager:
 		# Define a query to execute
 		metadata = ingested_track.metadata
 		blob = json.dumps(metadata)
-		query = f"""INSERT INTO ingested_tracks (title, artist, album, album_id, title_id, artist_id, metadata, source, timestamp, ingested_at, uri, user_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s ,%s, %s);"""
+		query = f"""INSERT INTO ingested_tracks (title, artist, album, album_id, title_id, artist_id, metadata, source, timestamp, ingested_at, uri, user_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s ,%s, %s) ON CONFLICT DO NOTHING;"""
 
 		try:
 			cur = self.conn.cursor()
@@ -41,3 +41,50 @@ class DBManager:
 			print(query)
 			input()
 
+	def select_all_artist_id(self):
+		# Define a query to execute
+		query = "SELECT distinct(artist_id) FROM ingested_tracks;"
+		try:
+			cur = self.conn.cursor()
+			cur.execute(query)
+			results = cur.fetchall()
+			return results
+		except psycopg2.Error as e:
+			print("Error executing query:", e)
+
+	def insert_stage_genre(self,artist_id, genres):
+
+		query = f"""INSERT INTO stage_genres (artist_id, genre) VALUES (%s, %s) ON CONFLICT DO NOTHING;"""
+		try: 
+			cur = self.conn.cursor()
+			cur.execute(query, (artist_id, genres))
+			self.conn.commit()
+			print("Query executed successfully!")
+		except psycopg2.Error as e:
+			print("Error executing query:", e)
+			print(query)
+			input()
+		     
+	
+	def select_filter(self, query):
+		try:
+			cur = self.conn.cursor()
+			cur.execute(query)
+			results = cur.fetchall()
+			return results
+		except psycopg2.Error as e:
+			print("Error executing query:", e)
+			print(query)
+			input()
+	
+	def __execute__(self, query):
+
+		try:
+			cur = self.conn.cursor()
+			cur.execute(query)
+			self.conn.commit()
+			print("Query executed successfully!")
+		except psycopg2.Error as e:
+			print("Error executing query:", e)
+			print(query)
+			input()
---
+++ migrations/0002_create_genre_stage.sql
@@ -0,0 +1,4 @@
+CREATE TABLE stage_genres (
+	artist_id VARCHAR NOT NULL UNIQUE,
+	genre VARCHAR NOT NULL
+);
